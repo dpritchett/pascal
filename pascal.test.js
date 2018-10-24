5ace5909a466a1b91a075e33a8a04614d9a7e5f4
Kenny <nagano564@gmail.com>
2018-10-23 20:13:01 -0400

    commit 2

---
+++ pascal.test.js
@@ -0,0 +1,8 @@
+const {assert} = require("chai");
+
+describe("pascal", function() {
+  it("basic tests", function() {
+    assert.deepEqual(pascal(1), [[1]]);
+    assert.deepEqual(pascal(4), [[1],[1,1],[1,2,1],[1,3,3,1]]);
+  });
+});
